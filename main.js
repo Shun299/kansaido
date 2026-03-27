cd09267fda678fe38b9f1514c207a009fb28f8a4
Shun299 <23610088ss@stu.yamato-u.ac.jp>
2026-03-27 18:10:41 +0900

    animation　追加

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const option = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.3,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('active')
+                observer.unobserve(entry.target);
+            }
+        })
+    },option);
+
+    document.querySelectorAll('.move_upwards, .right1, .left2, .active').forEach(el => observer.observe(el));
+})
