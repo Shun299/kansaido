74f172e53e041cf341d2404db4eb960e116976a1
Shun299 <23610088ss@stu.yamato-u.ac.jp>
2026-05-10 18:11:11 +0900

    Update 2

--- js/product.js
+++ js/product.js
@@ -17,33 +17,60 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
 
+        let hitCount = 0;
+        const hitText = document.querySelector("#hit-count");
+        const hitNumber = document.querySelector("#hit-number");
+
         productCard.forEach((card) => {
             const hasTypeCheck = typeChecklist.length > 0;
             const hasMethodCheck = methodChecklist.length > 0;
             const matchType = typeChecklist.includes(card.dataset.type);
             const matchMethod = methodChecklist.includes(card.dataset.method);
 
-            if(!hasTypeCheck && !hasMethodCheck) {
+            const isHit = 
+                (!hasTypeCheck && !hasMethodCheck) ||
+                (hasTypeCheck && !hasMethodCheck && matchType) ||
+                (!hasTypeCheck && hasMethodCheck && matchMethod) ||
+                (matchType && matchMethod);
+
+            if(isHit) {
                 card.classList.remove("remove");
-            } else if (hasTypeCheck && !hasMethodCheck) {
-                if(matchType) {
-                    card.classList.remove("remove");
-                } else {
-                    card.classList.add("remove");
-                }
-            } else if (!hasTypeCheck && hasMethodCheck) {
-                if(matchMethod) {
-                    card.classList.remove("remove");
-                } else {
-                    card.classList.add("remove");
-                }
+                hitCount++;
             } else {
-                if (matchType && matchMethod) {
-                    card.classList.remove("remove");
-                } else {
-                    card.classList.add("remove");
-                }
+                card.classList.add("remove");
             }
+
+            // if(!hasTypeCheck && !hasMethodCheck) {
+            //     card.classList.remove("remove");
+            //     hitCount++;
+            // } else if (hasTypeCheck && !hasMethodCheck) {
+            //     if(matchType) {
+            //         card.classList.remove("remove");
+            //         hitCount++;
+            //     } else {
+            //         card.classList.add("remove");
+            //     }
+            // } else if (!hasTypeCheck && hasMethodCheck) {
+            //     if(matchMethod) {
+            //         card.classList.remove("remove");
+            //         hitCount++;
+            //     } else {
+            //         card.classList.add("remove");
+            //     }
+            // } else {
+            //     if (matchType && matchMethod) {
+            //         card.classList.remove("remove");
+            //         hitCount++;
+            //     } else {
+            //         card.classList.add("remove");
+            //     }
+            // }
+            // hitCountText.textContent = `${hitCount}件の商品が見つかりました`;
         })
+        if(hitCount === 0) {
+            hitText.textContent = "条件に一致する商品は見つかりませんでした";
+        } else {
+            hitText.innerHTML = `<span id="hit-number">${hitCount} </span>件の商品が見つかりました`
+        }
     })
 })
