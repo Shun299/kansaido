d2defa7d02d1f40f19c92ffb61e2e885e609899d
Shun299 <23610088ss@stu.yamato-u.ac.jp>
2026-05-17 11:56:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,11 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelectorAll('.reco-card-move, .move_upwards, .right1, .left2, .blur , .active').forEach(el => observer.observe(el));
 
-    const loadingImg = document.querySelector("#loading__img");
-    loadingImg.classList.add("is-blur");
-
-    window.addEventListener("load", ()=> {
+    window.addEventListener("load", ()=> {    
         const loading = document.querySelector("#loading");
+        const loadingImg = document.querySelector("#loading__img");
+        loadingImg.classList.add("is-blur");
         setTimeout(() => {
             loading.classList.add("is-hidden");
         }, 800);
